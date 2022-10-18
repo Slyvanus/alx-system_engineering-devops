68cd099a48fc5b998dc351b143d06738aef3dc5b
slyvanus <mensahnaphtali18@gmail.com>
2022-10-18 16:22:13 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *main: entry point of this code
+ *this function prints putchar
+ *
+ * return: always 0
+ */
+
+int main(void)
+
+{
+
+	_putchar('-');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/*
+ *
+ *
+ *
+ */
+
+
+void print_alphabet(void)
+{ 
+	char alpha;
+	for (alpha ='a'; alpha<='z'; alpha++);
+		_putchar('\n');
+}
